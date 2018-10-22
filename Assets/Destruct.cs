f23efab546f24fc584c695ce1c08966c18d0535b
david <36555688+coledennis@users.noreply.github.com>
2018-10-22 16:53:51 -0700

    VR Set Up Initial coding

---
+++ Assets/Destruct.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruct : MonoBehaviour {
+
+    public GameObject destroyedVersion;
+
+   void OnTriggerEnter(Collider col)
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/TreeDestrutction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeDestrutction : MonoBehaviour {
+
+    public GameObject destroyedVersion;
+
+    private void Start()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("Trigger Pulled");
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}

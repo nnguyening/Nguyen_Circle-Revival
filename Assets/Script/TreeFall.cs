aa1ddca097508a2af6366950708803b788ef583e
david <36555688+coledennis@users.noreply.github.com>
2018-10-23 12:15:55 -0700

    CodingFix
    Stared on coding

--- Assets/Script/TreeFall.cs
+++ Assets/Script/TreeFall.cs
@@ -1,21 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreeDestrutction : MonoBehaviour {
+public class TreeFall : MonoBehaviour {
 
     public GameObject destroyedVersion;
 
-    private void Start()
-    {
-        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
-        {
-            Debug.Log("Trigger Pulled");
-        }
-    }
     void OnTriggerEnter(Collider col)
     {
         Instantiate(destroyedVersion, transform.position, transform.rotation);
         Destroy(gameObject);
     }
+
 }

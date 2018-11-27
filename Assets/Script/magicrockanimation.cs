6a9f2d53ec35987259f5bc806821cad1d3736a8f
david <36555688+coledennis@users.noreply.github.com>
2018-11-26 16:50:59 -0800

    Final Build

---
+++ Assets/Script/magicrockanimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magicrockanimation : MonoBehaviour
+{
+
+    public Animation stopanimation;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if ("OnRightHandAnchor" == col.gameObject.name)
+        {
+            stopanimation.Stop();
+        }
+    }
+}
